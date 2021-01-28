ad2ceb52515cf14f6a62fe5675b5032257abdd83
Ranjit029 <ranjitbehera029@gmail.com>
2021-01-28 23:32:34 +0530

    Final Commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Farmers',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Farmer2',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Farmer2',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Farmer2',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Farmer2',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Farmer2',
+    image: '/images/',
+    description:
+      'Use this',
+    brand: 'Local',
+    category: 'Local',
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
